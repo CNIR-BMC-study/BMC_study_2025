ca010e91883b386acfa062761ddb9822753bf51e
Hoyeon Jang <cliched.0-hoedown@icloud.com>
2025-03-28 14:43:45 +0900

    add Gaussian likelihood

--- src/HBM/CRP.py
+++ src/HBM/CRP.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.special as sp
+from scipy.stats import multivariate_normal
 from .structures import Feature, Category
 
 class CRP:
@@ -7,13 +8,15 @@ class CRP:
         self,
         alpha = 1.0,
         delta = 1.0,
-        strict: bool = False
+        strict: bool = False,
+        type: str = "beta"
     ) -> None:
         self._alpha = alpha
         self._delta = delta
         self._strict = strict
         self._n = 0
         self._clusters = []
+        self._type = type
 
     @property
     def clusters(self) -> list:
@@ -22,7 +25,7 @@ class CRP:
     def add(self, feature: Feature) -> int:
         self._n += 1
         ll = np.array(
-            [self.ll(cluster, feature, "beta") for cluster in self._clusters]
+            [self.ll(cluster, feature, self._type) for cluster in self._clusters]
         )
         prior = self.prior()
         p = np.append(
@@ -42,6 +45,8 @@ class CRP:
     def ll(self, clusters, feature: Feature, type: str = "beta"):
         if type == "beta":
             return self.beta_ll(clusters, feature)
+        elif type == "gaussian":
+            return self.gaussian_ll(clusters, feature)
 
     def beta_ll(self, cluster, feature: Feature):
         if not self._clusters:
@@ -57,6 +62,23 @@ class CRP:
             cluster.n - n_class_features + self._delta
         ) / sp.beta(self._delta, self._delta)
 
+    def gaussian_ll(self, cluster, feature: Feature):
+        if not self._clusters:
+            return 1e-8
+        return multivariate_normal.pdf(feature.values, cluster.mean(), cluster.cov(), allow_singular=True)
+
+    def baysian_ll(self, cluster, feature: Feature):
+        if not self._clusters:
+            return 1e-8
+        s = cluster.cov() * cluster.n if cluster.n > 1 else np.zeors((feature.n, feature.n))
+        kappa_n = cluster.kappa0 + cluster.n
+        nu_n = cluster.nu0 + cluster.n
+        mu_n = (cluster.kappa0 * cluster.mean_prior + cluster.n * cluster.mean()) / kappa_n
+        psi_n = cluster.psi0 + s + (cluster.kappa0 * cluster.n) / (cluster.kapaa0 + cluster.n) * np.outer(cluster.mean() - cluster.mean_prior, cluster.mean() - cluster.mean_prior)
+        cov_pred = psi_n * (kappa_n + 1) / (kappa_n * (nu_n - feature.n + 1))
+        return multivariate_normal.pdf(feature.values, mu_n, cov_pred, allow_singular=True)
+
+
     def prior(self) -> np.ndarray:
         return np.array([
             i.n / (self._n + self._alpha -1) for i in self._clusters
@@ -68,9 +90,10 @@ class StickyCRP(CRP):
         alpha = 1.0,
         delta = 1.0,
         strict: bool = False,
-        stickiness = 1.0
+        stickiness = 1.0,
+        type:str = "beta"
     ) -> None:
-        super().__init__(alpha, delta, strict)
+        super().__init__(alpha, delta, strict, type)
         self._stickiness = stickiness
         self._memory = 0
 
--- src/HBM/structures.py
+++ src/HBM/structures.py
@@ -22,6 +22,7 @@ class Category:
     def __init__(self) -> None:
         self._n = 0
         self._items = []
+        self._kappa0 = 1e-3
 
     @property
     def n(self):
@@ -31,6 +32,31 @@ class Category:
     def items(self):
         return self._items
 
-    def add(self, features: Feature):
+    @property
+    def kappa0(self):
+        return self._kappa0
+
+    @property
+    def nu0(self):
+        return self._nu0
+
+    @property
+    def psi0(self):
+        return self._psi0
+
+    @property
+    def mean_prior(self):
+        return self._mean_prior
+
+    def add(self, feature: Feature):
         self._n += 1
-        self._items.append(features)
+        self._items.append(feature)
+        self._nu0 = feature.n + 1
+        self._psi0 = np.eye(feature.n)
+        self._mean_prior = np.zeros(feature.n)
+
+    def mean(self):
+        return np.mean([i.values for i in self._items], axis=0)
+
+    def cov(self):
+        return np.cov([i.values for i in self._items], rowvar=False)
